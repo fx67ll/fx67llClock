dae9ea40ada8cece340e1611b83b30d46a6fe301
fx67ll <2717745014@qq.com>
2021-12-17 15:27:08 +0800

    format

---
+++ src/components/flip-clock/flip-clock.js
@@ -0,0 +1,8 @@
+import fx67llFlipClock from './flip-clock.vue';
+fx67llFlipClock.install = function(Vue) {
+	Vue.component(fx67llFlipClock.name, fx67llFlipClock);
+};
+export {
+	default as fx67llFlipClock
+}
+from './flip-clock.vue'
---
+++ src/components/flip-clock/flip-clock.vue
@@ -0,0 +1,33 @@
+<template>
+	<!-- <div class="fx67ll-clock-box">
+		<div class="fx67ll-clock-card-box">
+			<div class="fx67ll-clock-card"></div>
+		</div>
+	</div> -->
+
+	<div class="stage">
+		<div class="box">
+			<div class="box-prev"></div>
+			<div class="box-midd"></div>
+			<div class="box-next">
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'fx67llFlipClock',
+	data() {
+		return {};
+	},
+	methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+@import '@a/styles/flip-clock/flip-clock.less';
+// @import '@a/styles/flip-clock/flip-clock-back.less';
+</style>
