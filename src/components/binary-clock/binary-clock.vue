1c15cf5fd6e11eeba0a4ec2c96f43377bc092d3f
fx67ll <2717745014@qq.com>
2021-12-22 17:18:27 +0800

    finish first version of the binary clock

--- src/components/binary-clock/binary-clock.vue
+++ src/components/binary-clock/binary-clock.vue
@@ -1,31 +1,10 @@
 <template>
-	<div class="center">
-		<p></p>
+	<div class="bc-box">
+		<p>{{ timeNow }}</p>
 		<table>
-			<tr class="hours">
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr>
-			<tr class="minutes">
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr>
-			<tr class="seconds">
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr>
+			<tr class="bc-hours"><td v-for="(item, i) in hoursTimeArr" :key="i" :class="{ num0: item === '0', num1: item == '1' }"></td></tr>
+			<tr class="bc-minutes"><td v-for="(item, i) in minutesTimeArr" :key="i" :class="{ num0: item === '0', num1: item == '1' }"></td></tr>
+			<tr class="bc-seconds"><td v-for="(item, i) in secondsTimeArr" :key="i" :class="{ num0: item === '0', num1: item == '1' }"></td></tr>
 		</table>
 	</div>
 </template>
@@ -34,9 +13,63 @@
 export default {
 	name: 'fx67llBinaryClock',
 	data() {
-		return {};
+		return {
+			// 当前时间
+			timeNow: null,
+			// 计时器对象
+			timer: null,
+			// 小时二进制数组
+			hoursTimeArr: [0, 0, 0, 0, 0, 0],
+			// 分钟二进制数组
+			minutesTimeArr: [0, 0, 0, 0, 0, 0],
+			// 秒钟二进制数组
+			secondsTimeArr: [0, 0, 0, 0, 0, 0]
+		};
 	},
-	methods: {}
+	mounted() {
+		var self = this;
+		this.timer = setInterval(function() {
+			self.getNowTime();
+		}, 1000);
+	},
+	destroyed() {
+		clearInterval(this.timer);
+		this.timer = null;
+	},
+	methods: {
+		// 获取当前时间
+		getNowTime() {
+			let d = new Date();
+			let h = d.getHours();
+			let m = d.getMinutes();
+			let s = d.getSeconds();
+			this.timeNow = this.addZero(h) + ' : ' + this.addZero(m) + ' : ' + this.addZero(s);
+			this.hoursTimeArr = this.convertTenToTwo(h);
+			this.minutesTimeArr = this.convertTenToTwo(m);
+			this.secondsTimeArr = this.convertTenToTwo(s);
+		},
+		// 补齐时间格式
+		addZero(i) {
+			if (i < 10) {
+				i = '0' + i;
+			}
+			return i;
+		},
+		// 十进制数字转换为二进制
+		convertTenToTwo(num) {
+			let bin = '';
+			let convArr = [];
+			while (num > 0) {
+				bin = (num % 2) + bin;
+				num = Math.floor(num / 2);
+			}
+			convArr = bin.split('');
+			while (convArr.length < 6) {
+				convArr.unshift('0');
+			}
+			return convArr;
+		}
+	}
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ Vue.config.productionTip = false
 import less from 'less'
 Vue.use(less)
 
-import fx67llClock from '@c/index';
-Vue.use(fx67llClock);
+// import fx67llClock from '@c/index';
+// Vue.use(fx67llClock);
 
 new Vue({
 	router,
